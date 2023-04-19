83b57bb89a429e3af9362e8511b6ce96d97a21c5
yangling <2397328561@qq.com>
2023-04-19 17:00:02 +0800

    fix(修改gateway客户端与服务端):
    server需要对用户的注册，登录，踢走，消息转发等功能进行实现

--- game-server/business/server/gateway/client/client.go
+++ game-server/business/server/gateway/client/client.go
@@ -7,19 +7,7 @@ Server
 @Description: 服务：维护客户端连接
 */
 type Server struct {
-	clients map[int64]*network.Client // 维护客户端连接
-}
-
-func (s *Server) AddClient(client *network.Client) {
-	s.clients[client.ConnID] = client
-}
-
-func (s *Server) DelClient(client *network.Client) {
-	delete(s.clients, client.ConnID)
-}
-
-func (s *Server) GetServerCount() int32 {
-	return int32(len(s.clients))
+	real *network.Server
 }
 
 func (s *Server) Loop() {
@@ -30,3 +18,16 @@ func (s *Server) Loop() {
 		}
 	}
 }
+
+//clients map[int64]*network.Client // 维护客户端连接
+//func (s *Server) AddClient(client *network.Client) {
+//	s.clients[client.ConnID] = client
+//}
+//
+//func (s *Server) DelClient(client *network.Client) {
+//	delete(s.clients, client.ConnID)
+//}
+//
+//func (s *Server) GetServerCount() int32 {
+//	return int32(len(s.clients))
+//}
--- game-server/business/server/gateway/server/server.go
+++ game-server/business/server/gateway/server/server.go
@@ -7,27 +7,54 @@ Server
 @Description: 服务：维护服务端连接
 */
 type Server struct {
-	clients map[int64]*network.Client
+	real *network.Server
 }
 
-func (s *Server) AddClient(client *network.Client) {
-	s.clients[client.ConnID] = client
+func (s *Server) KickUser() {
+
 }
 
-func (s *Server) DelClient(client *network.Client) {
-	delete(s.clients, client.ConnID)
+func (s *Server) KickAllUser() {
+
 }
 
 /*
-GetServerCount
-@Description: 获取服务端数量
+TransMessageToGateway
+@Description: 将消息传递到网关
 @receiver s
-@return int32
 */
-func (s *Server) GetServerCount() int32 {
-	return int32(len(s.clients))
+func (s *Server) TransMessageToGateway() {
+
 }
 
+/*
+UpdateRegister
+@Description: 更新注册
+@receiver s
+*/
+func (s *Server) UpdateRegister() {
+
+}
+
+/*
+CheckRegister
+@Description: 检测注册
+@receiver s
+@return bool
+*/
+func (s *Server) CheckRegister() bool {
+	return true
+}
+
+func (s *Server) ReLoginAll() {
+
+}
+
+/*
+Loop
+@Description: 轮询事件
+@receiver s
+*/
 func (s *Server) Loop() {
 
 	for {
@@ -37,3 +64,23 @@ func (s *Server) Loop() {
 		}
 	}
 }
+
+//clients map[int64]*network.Client
+
+//func (s *Server) AddClient(client *network.Client) {
+//	s.clients[client.ConnID] = client
+//}
+//
+//func (s *Server) DelClient(client *network.Client) {
+//	delete(s.clients, client.ConnID)
+//}
+//
+///*
+//GetServerCount
+//@Description: 获取服务端数量
+//@receiver s
+//@return int32
+//*/
+//func (s *Server) GetServerCount() int32 {
+//	return int32(len(s.clients))
+//}
